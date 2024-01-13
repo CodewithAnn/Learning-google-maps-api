485ba72f3ca259723b85193e6aa54f2d701214f8
Anand jha <codewithann@gmail.com>
2024-01-13 08:42:59 +0530

    added initial coordinates
        - use _kcamraPostion to create initial coordinates
        - create instance of GoogleMapsController
        - further include markers

--- lib/home_Page.dart
+++ lib/home_Page.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,6 +11,9 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  static const CameraPosition _kcamraPosition = CameraPosition(
+      target: LatLng(28.749709620347968, 77.11836119500983), zoom: 14.4416);
+  final Completer<GoogleMapController> _controller = Completer();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,6 +21,14 @@ class _HomePageState extends State<HomePage> {
         title: const Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [Text('Google Maps')]),
+      ),
+      body: SafeArea(
+        child: GoogleMap(
+          initialCameraPosition: _kcamraPosition,
+          onMapCreated: (controller) {
+            _controller.complete(controller);
+          },
+        ),
       ),
     );
   }
