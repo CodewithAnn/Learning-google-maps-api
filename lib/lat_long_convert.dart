093b4f4700b3640e6ccaff63416bb35ca95ace55
Anand jha <codewithann@gmail.com>
2024-01-14 10:21:57 +0530

    convert Lat/long into address
        - Learned how to convert with geocoder package
        - coordinates convert into address and printed yet into consol
        - yet still need to show on screen later

--- lib/lat_long_convert.dart
+++ lib/lat_long_convert.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_geocoder/geocoder.dart';
 
 class LatLongToAdd extends StatefulWidget {
   const LatLongToAdd({super.key});
@@ -10,6 +11,35 @@ class LatLongToAdd extends StatefulWidget {
 class _LatLongToAddState extends State<LatLongToAdd> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Convert Lat/Long "),
+        centerTitle: true,
+      ),
+      body: SafeArea(
+          child: Center(
+        child: ElevatedButton(
+            onPressed: () async {
+              // For convert from  coordinate to Address(lat/long to Address)
+              final _coordinate =
+                  Coordinates(47.867519381885856, 12.107215213491571);
+              var _address = await Geocoder.local
+                  .findAddressesFromCoordinates(_coordinate);
+              var first = _address.first;
+
+              print("Address :" +
+                  first.featureName.toString() +
+                  "" +
+                  first.addressLine.toString());
+
+              // For Convert to coordinate from address(address to Lat/Long)
+              // final query = "Hochschulstraße 1, 83024 Rosenheim, Germany";
+              // var _coordinates =
+              //     await Geocoder.local.findAddressesFromQuery(query);
+              // var latLong = _coordinates.first;
+            },
+            child: Text("Convert")),
+      )),
+    );
   }
 }
