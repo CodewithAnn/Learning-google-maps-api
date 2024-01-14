6ea09a3a73c115aad964885e0b586be556fd9bfc
Anand jha <codewithann@gmail.com>
2024-01-14 10:43:02 +0530

    showed address on screen
        - use setState to show the address to update an empty starting
          in card widget

--- lib/lat_long_convert.dart
+++ lib/lat_long_convert.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_geocoder/geocoder.dart';
 
@@ -9,6 +11,7 @@ class LatLongToAdd extends StatefulWidget {
 }
 
 class _LatLongToAddState extends State<LatLongToAdd> {
+  String showAddress = "";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,28 +21,48 @@ class _LatLongToAddState extends State<LatLongToAdd> {
       ),
       body: SafeArea(
           child: Center(
-        child: ElevatedButton(
-            onPressed: () async {
-              // For convert from  coordinate to Address(lat/long to Address)
-              final _coordinate =
-                  Coordinates(47.867519381885856, 12.107215213491571);
-              var _address = await Geocoder.local
-                  .findAddressesFromCoordinates(_coordinate);
-              var first = _address.first;
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Card(
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Text(showAddress),
+              ),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.089,
+            ),
+            ElevatedButton(
+                onPressed: () async {
+                  // For convert from  coordinate to Address(lat/long to Address)
+                  final _coordinate =
+                      Coordinates(47.867519381885856, 12.107215213491571);
+                  var _address = await Geocoder.local
+                      .findAddressesFromCoordinates(_coordinate);
+                  var first = _address.first;
 
-              print("Address : " +
-                  // first.featureName.toString() +
-                  "" +
-                  first.addressLine.toString());
+                  print("Address : " +
+                      // first.featureName.toString() +
+                      "" +
+                      first.addressLine.toString());
 
-              // For Convert to coordinate from address(address to Lat/Long)
-              final query = "Hochschulstraße 1, 83024 Rosenheim, Germany";
-              var _coordinates =
-                  await Geocoder.local.findAddressesFromQuery(query);
-              var latLong = _coordinates.first;
-              print("Lat/Long : " + _coordinates.first.coordinates.toString());
-            },
-            child: Text("Convert")),
+                  // For Convert to coordinate from address(address to Lat/Long)
+                  const query = "Hochschulstraße 1, 83024 Rosenheim, Germany";
+                  var _coordinates =
+                      await Geocoder.local.findAddressesFromQuery(query);
+                  var latLong = _coordinates.first;
+                  print("Lat/Long : " +
+                      _coordinates.first.coordinates.toString());
+
+                  setState(() {
+                    showAddress = first.addressLine.toString();
+                  });
+                },
+                child: Text("Convert")),
+          ],
+        ),
       )),
     );
   }
