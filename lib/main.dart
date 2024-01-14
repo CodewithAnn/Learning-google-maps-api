d70a4660997fe705ac919e5986ea82f558ddf758
Anand jha <codewithann@gmail.com>
2024-01-14 09:55:53 +0530

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_api/home_Page.dart';
+import 'package:google_maps_api/lat_long_convert.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomePage(),
+      home: const LatLongToAdd(),
     );
   }
 }
