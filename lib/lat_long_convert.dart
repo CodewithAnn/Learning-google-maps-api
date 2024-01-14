5f628fe13ce3f15c285434277df85339a0aab0d0
Anand jha <codewithann@gmail.com>
2024-01-14 09:55:12 +0530

    added file

---
+++ lib/lat_long_convert.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LatLongToAdd extends StatefulWidget {
+  const LatLongToAdd({super.key});
+
+  @override
+  State<LatLongToAdd> createState() => _LatLongToAddState();
+}
+
+class _LatLongToAddState extends State<LatLongToAdd> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
